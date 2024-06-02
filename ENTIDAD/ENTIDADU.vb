d78e7e0ef58eb76203280014d1725d38a9d58d59
Orsade <63831224+davbr12@users.noreply.github.com>
2024-06-01 19:08:52 -0600

    ENTIDAD
    ENTIDAD DE LA APLICACIÓN

---
+++ ENTIDAD/ENTIDADU.vb
@@ -0,0 +1,33 @@
+﻿Public Class ENTIDADU
+    Private CORREO As String
+    Private CONTRASEÑA As String
+    Private IDTIPOU As String
+    'agregar referencias en datos a entidad
+    Public Property _CORREO
+        Get
+            Return CORREO
+        End Get
+        Set(value)
+            CORREO = value
+        End Set
+    End Property
+    Public Property _CONTRASEÑA
+        Get
+            Return CONTRASEÑA
+        End Get
+        Set(value)
+            CONTRASEÑA = value
+        End Set
+    End Property
+    Public Property _IDTIPOU
+        Get
+            Return IDTIPOU
+        End Get
+        Set(value)
+            IDTIPOU = value
+        End Set
+    End Property
+    Public Sub New()
+    End Sub
+End Class
+
